ebd96e6a47d13eb5232cdcc029e596a4a04a815f
Sasni Lasadi <sasnilasadi410@gmail.com>
2026-09-21 01:09:54 +0530

    Wrap app in CmsProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { CmsProvider } from "@/context/CmsContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +31,13 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col bg-[#0F172A] text-white font-sans">
-        <Navbar />
-        <div className="flex-1">
-          {children}
-        </div>
-       <Footer />
+        <CmsProvider>
+          <Navbar />
+          <div className="flex-1">
+            {children}
+          </div>
+          <Footer />
+        </CmsProvider>
       </body>
     </html>
   );
